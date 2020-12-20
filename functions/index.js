2306d99336fe2c75f2b88573c2b6a85d4e73bf73
Rahadian Yusuf <rahadian.y@gmail.com>
2020-12-20 20:22:43 +0700

    add express & body parser

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,12 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+app.use(bodyParser.json());
+
+const todolistRouter = express.Router();
+
+app.use('/api', todolistRouter);
+
+exports.todolist = functions.https.onRequest(app);
