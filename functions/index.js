0ae32c8e5758953b109de1bef542abccca44a2e1
Rahadian Yusuf <rahadian.y@gmail.com>
2020-12-20 20:37:31 +0700

    add cru

--- functions/index.js
+++ functions/index.js
@@ -12,6 +12,50 @@ app.use(bodyParser.json());
 
 const todolistRouter = express.Router();
 
+todolistRouter.get('/todos', async (req, res) => {
+  await db.ref('/todos').once('value').then((snapshot) => {
+    res.json(snapshot.val());
+  });
+});
+
+todolistRouter.get('/todos/:id', async (req, res) => {
+  await db.ref('/todos')
+    .orderByKey()
+    .equalTo(req.params.id)
+    .once('value')
+    .then((snapshot) => {
+      res.json(snapshot.val());
+    });
+});
+
+todolistRouter.post('/todos', async (req, res) => {
+  const { todo } = req.body;
+  const todoListsRef = db.ref('/todos');
+  await todoListsRef.push({
+    todo,
+    is_marked_done: false
+  })
+
+  await db.ref('/todos').once('value').then((snapshot) => {
+    res.json(snapshot.val());
+  });
+});
+
+todolistRouter.patch('/todos/:id/toggle-mark', async (req, res) => {
+  const { id } = req.params;
+
+  const data = (await db.ref('/todos')
+    .orderByKey()
+    .equalTo(id)
+    .once('value')).val()[id];
+  data.is_marked_done = !data.is_marked_done;
+
+  await db.ref(`/todos/${id}`).set(data);
+  res.json({
+    id: data,
+  });
+});
+
 app.use('/api', todolistRouter);
 
 exports.todolist = functions.https.onRequest(app);
