1555a1fb94b2921247bfef697b8bde0c455f05ce
Rahadian Yusuf <rahadian.y@gmail.com>
2020-12-21 20:58:41 +0700

    init add components

---
+++ ui/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Test
+  </div>
+</template>
+
+<script type="ts">
+import api from './api'
+
+export default {
+  name: 'App',
+  data: () => ({
+    todos: []
+  }),
+  async created() {
+    this.todos = await api.getTodos();
+  }
+}
+</script>
